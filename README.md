5bf3f1781ffb60950fe888ba06bd8c007f7add14
afeiship <afeiship@gmail.com>
2019-01-15 08:59:49 +0800

    feat: init & done

---
+++ README.md
@@ -0,0 +1,12 @@
+# next-is-array
+> ArrayIsArray polly fill for next
+
+## install:
+```bash
+npm install -S afeiship/next-is-array --registry=https://registry.npm.taobao.org
+```
+
+## usage:
+```js
+//DOCS here!
+```
---
+++ src/next-is-array.js
@@ -0,0 +1,14 @@
+(function() {
+  var global = global || this || window || Function('return this')();
+  var nx = global.nx || require('next-js-core2');
+
+  nx.isArray =
+    Array.isArray ||
+    function(inObj) {
+      return inObj instanceof Array;
+    };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.isArray;
+  }
+})();
